14409a33a0dd39bbdf066d705bc0933c681059c1
AyumiWatanabe <260193154+miy0ug1y-spec@users.noreply.github.com>
2026-04-02 07:22:25 +0000

    [Finish]2-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
     flash[:notice] = "Book was successfully created."
-    redirect_to '/books/:id/edit'
+    redirect_to book_path(@book.id)
     else
     @books = Book.all.order(created_at: :desc)
     render :index, status: :unprocessable_entity
@@ -16,20 +16,23 @@ class BooksController < ApplicationController
   end
 
   def show
-    @books = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
-    @books = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully Updated."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully Updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all.order(created_at: :asc)
+      render :edit, status: :unprocessable_entity
+    end
   end
-
 
   def destroy
      book = Book.find(params[:id])
